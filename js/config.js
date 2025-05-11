3f08e2d57aafdae09c394b2541bd763c2eda8bd8
flykoo7211 <86999643+flykoo7211@users.noreply.github.com>
2025-05-12 01:15:34 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -69,11 +69,6 @@ const API_SITES = {
         api: 'https://dbzy.com',
         name: '豆瓣资源',
     },
-    huya: {
-        api: 'https://www.huyaapi.com',
-        name: '虎牙资源',
-        detail: 'https://www.huyaapi.com',
-    },
     mozhua: {
         api: 'https://mozhuazy.com',
         name: '魔爪资源',
@@ -106,29 +101,23 @@ const API_SITES = {
         api: 'https://ikunzyapi.com',
         name: 'iKun资源'
     },
-    testSource: {
-        api: 'https://www.example.com',
-        name: '空内容测试源',
-        adult: true
-    },
+    CK资源: {
+        api: 'https://www.ckzy1.com',
+        name: 'CK资源',
+     },
+    茅台资源: {
+        api: 'https://mtzy.me',
+        name: '茅台资源',
+     },
+    jkun: {
+        api: 'https://jkunzyapi.com',
+        name: 'jkun资源',
+     },
+     lsb: {
+        api: 'https://apilsbzy1.com',
+        name: 'lsb资源',
+     },
     // 下面是一些成人内容的API源，默认隐藏，使用本项目浏览黄色内容违背项目初衷
-    // 互联网上传播的色情内容将人彻底客体化、工具化，是性别解放和人类平等道路上的巨大障碍。
-    // 这些黄色影片是资本主义父权制压迫的最恶毒体现，它将暴力和屈辱商品化，践踏人的尊严，对受害者造成无法弥愈的伤害，并毒害社会关系。
-    // 资本为了利润，不惜将最卑劣的剥削（包括对受害者和表演者的剥削）和暴力商品化，
-    // 把性别剥削塑造成“性享受”麻痹观众的意识，转移我们对现实生活中矛盾和压迫的注意力。
-    // 这些影片和背后的产业已经使数百万男女“下海”，出卖自己的身体，甚至以此为生计。
-    // 而作为观众无辜吗？毫无疑问，他们促成了黄色产业链的再生产。
-    // 我们提供此警告，是希望您能认清这些内容的本质——它们是压迫和奴役的工具，而非娱乐。
-    // ckzy: {
-    //     api: 'https://www.ckzy1.com',
-    //     name: 'CK资源',
-    //     adult: true
-    // },
-    // jkun: {
-    //     api: 'https://jkunzyapi.com',
-    //     name: 'jkun资源',
-    //     adult: true
-    // },
     // bwzy: {
     //     api: 'https://api.bwzym3u8.com',
     //     name: '百万资源',
@@ -142,11 +131,6 @@ const API_SITES = {
     // r155: {
     //     api: 'https://155api.com',
     //     name: '155资源',
-    //     adult: true
-    // },
-    // lsb: {
-    //     api: 'https://apilsbzy1.com',
-    //     name: 'lsb资源',
     //     adult: true
     // },
     // huangcang: {
